70a5d64b14101c9536581f497e000379128e51fa
TehEpikDuckeh <tehepikduck@live.com>
2013-05-25 18:21:33 -0600

    Create AB_backup.sh

---
+++ AB_backup.sh
@@ -0,0 +1,8 @@
+print("Welcome to AB_Backup!")
+print("Would you like to begin?")
+if input == "yes","Yes" then
+print("Ok, one second while I begin!")
+sleep(2)
+print("Starting backup! Do not affect the Pi in any way...")
+sleep(1)
+print("Backing Up!")
